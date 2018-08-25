4659cda19b15e5fbb9d5d386d804b2bac7b70dff
Cody S <sheridan.codyj@gmail.com>
2018-08-25 08:13:11 -0400

    08-24-18 2 Complete.

--- re-082418.js
+++ re-082418.js
@@ -1,3 +1,9 @@
+// RegEx - Using the Test Method
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
+
 // Match Literal Strings
 
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
--- re-assignment-01.js
+++
@@ -1,5 +0,0 @@
-// RegEx - Using the Test Method
-
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-let result = myRegex.test(myString);

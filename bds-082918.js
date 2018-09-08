ef940a27bdf4af64221515c6bc4dbb558dcd8a1b
Cody S <sheridan.codyj@gmail.com>
2018-09-08 08:45:59 -0400

    08-29-18 3 complete

---
+++ bds-082918.js
@@ -0,0 +1,48 @@
+// Modify an Object Nested Within an Object
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+// change code below this line
+userActivity.data.online = 45;
+// change code above this line
+console.log(userActivity);
+
+//Access Property Names with Bracket Notation
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+// do not change code above this line
+function checkInventory(scannedItem) {
+  // change code below this line
+  return foods[scannedItem];
+}
+// change code below this line to test different cases:
+console.log(checkInventory("apples"));
+
+//Use the delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// change code above this line
+
+console.log(foods);
